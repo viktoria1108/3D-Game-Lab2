1a12f34f6c88f16b5e449862ac832f2d75ce62d6
Vik_toria <72942985+viktoria1108@users.noreply.github.com>
2022-09-25 16:55:39 +0300

    Player

---
+++ 3D-Game-Lab2/Assets/Scripts/SceneObjects/Player/PlayerLookScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class PlayerLookScript : MonoBehaviour
+{
+
+	
+	public enum RotationAxis {
+		MouseX = 1,
+		MouseY = 2
+	}
+
+	public RotationAxis axes = RotationAxis.MouseX;
+
+	public float minimumVert = -85.0f;
+	public float maximumVert = 85.0f;
+
+	public float sensHorizontal = 10.0f;
+	public float sensVertical = 10.0f;
+
+	private float _rotationX = 0;
+
+	public Camera playerCameraComponent;
+
+
+
+	private void Update() {
+        
+		if(true) {
+			CalculateLookRotation();
+		}
+	}
+
+	
+
+	private void CalculateLookRotation() {
+		if (axes == RotationAxis.MouseX) {
+			transform.Rotate(0, Input.GetAxis("Mouse X") * sensHorizontal, 0);
+		} else if (axes == RotationAxis.MouseY) {
+			_rotationX -= Input.GetAxis("Mouse Y") * sensVertical;
+			
+			_rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+			float rotationY = transform.localEulerAngles.y;
+			transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+		}
+	}
+
+
+}
---
+++ 3D-Game-Lab2/Assets/Scripts/SceneObjects/Player/PlayerMovementScript.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+
+	public float moveSpeed = 10.0f;
+	public float jumpSpeed = 4.0f;
+	public float gravity = 9.8f;
+	public float terminalVelocity = 100f;
+
+	private CharacterController _charCont;
+	private Vector3 _moveDirection = Vector3.zero;
+
+
+	void Start() {
+		_charCont = GetComponent<CharacterController>();
+	}
+
+	void Update() {
+       
+		if(true) {
+			HandlePlayerMove();
+		} else {
+			HandlePlayerInactiveMove();
+		}
+	}
+
+	private void HandlePlayerMove() {
+		
+		float deltaX = Input.GetAxis("Horizontal") * moveSpeed;
+		float deltaZ = Input.GetAxis("Vertical") * moveSpeed;
+		_moveDirection = new Vector3(deltaX, _moveDirection.y, deltaZ);
+		
+		if (_charCont.isGrounded) {
+			if (Input.GetButton("Jump")) {
+				_moveDirection.y = jumpSpeed;
+			} else {
+				_moveDirection.y = 0f;
+			}
+			
+			if(deltaX != 0 || deltaZ != 0) {
+				
+			}
+		} else {
+			
+		}
+		ApplyMovement();
+	}
+
+	private void HandlePlayerInactiveMove() {
+		_moveDirection = Vector3.zero;
+		ApplyMovement();
+	}
+
+	private void ApplyMovement() {
+		_moveDirection = transform.TransformDirection(_moveDirection);
+		
+		_moveDirection.y -= this.gravity * Time.deltaTime;
+		
+		_charCont.Move(_moveDirection * Time.deltaTime);
+	}
+
+
+}
